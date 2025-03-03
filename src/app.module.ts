21c277f825a93f27ede144cc3bdef570ab0bec22
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-03-03 13:46:45 +0500

    update: App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
+// console.log('🚀 MONGO_URL:', process.env.MONGO_URL); // Debug log
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URL || 'mongodb://localhost:27017/nestjs-api',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    const dbUrl = this.configService.get<string>('MONGO_URL');
+    return `Hello! Your database is running at: ${dbUrl}`;
+    //return 'Hello World!';
   }
 }
